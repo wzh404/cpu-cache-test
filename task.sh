837c841aa2859c839d8d350ab1b9919705027191
wangzunhui <wangzunhui2020@163.com>
2022-01-21 10:37:08 +0800

    linux shell

--- task.sh
+++ task.sh
@@ -3,6 +3,7 @@ buffer_size=1
 stride=64
 
 for i in `seq 1 15`; do
+#   taskset -ac 0 $work -b $buffer_size -s $stride
     $work -b $buffer_size -s $stride
     buffer_size=$(($buffer_size*2))
 done
